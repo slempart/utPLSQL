e2315063d9458aa3205612da7c69b34fbd406066
Vinicius Avellar Moreira <amvinicius@gmail.com>
2017-03-17 09:09:37 -0300

    Testing on circle v1.

--- .circleci/build_oracle_12c.sh
+++ .circleci/build_oracle_12c.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 set -e
 
-if [ -f ./cache/oracle-12c.tar.gz ]; then
+if [ -f $CACHE_DIR/oracle-12c.tar.gz ]; then
     echo "Oracle 12c is already cached, nothing to do..."
     exit 0
 fi
 
-cd $WORKDIR/.circleci
+cd $HOME/.circleci
 
 # Download Oracle 12c Install Files
 ORACLE12c_FILE1=linuxamd64_12102_database_se2_1of2.zip
@@ -17,7 +17,7 @@ mv $ORACLE12c_FILE2 ./dockerfiles/12.1.0.2
 
 # Build and Save Docker image
 cd ./dockerfiles/12.1.0.2
-docker build -f Dockerfile.se2 --force-rm -t oracle-12c-install .
+docker build --rm=false -f Dockerfile.se2 -t oracle-12c-install .
 docker run -d --privileged --name oracle-12c-install -p 1521:1521 oracle-12c-install
 docker logs -f oracle-12c-install | grep -m 1 "DATABASE IS READY TO USE!" --line-buffered
 docker exec oracle-12c-install ./setPassword.sh oracle
