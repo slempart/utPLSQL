7f02a816c174c542b9514f41b8ec418f20b5d5dc
Vinicius Avellar <amvinicius@gmail.com>
2017-03-16 23:13:35 -0300

    Trying Circle CI 2.0.

---
+++ .circleci/build_oracle_11g.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+set -e
+
+if [ -f ./cache/oracle-11g.tar.gz ]; then
+    echo "Oracle 11g is already cached, nothing to do..."
+    exit 0
+fi
+
+cd $WORKDIR/.circleci
+
+# Download Oracle 11g Install Files
+ORACLE11g_FILE=oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+bash download.sh -p xe11g
+mv $ORACLE11g_FILE ./dockerfiles/11.2.0.2
+
+# Build and Save Docker image
+cd ./dockerfiles/11.2.0.2
+docker build -f Dockerfile.xe --force-rm -t oracle-11g-install .
+docker run -d --privileged --shm-size=1g --name oracle-11g-install -p 1521:1521 oracle-11g-install
+docker logs -f oracle-11g-install | grep -m 1 "DATABASE IS READY TO USE!" --line-buffered
+docker exec oracle-11g-install ./setPassword.sh oracle
+docker stop oracle-11g-install
+docker commit oracle-11g-prebuilt
+docker save oracle-11g-prebuilt > ~/utplsql/.cache/oracle-11g.tar
+docker rm oracle-11g-install
+docker rmi oracle-11g-prebuilt
+docker rmi oracle-11g-install
---
+++ .circleci/build_oracle_12c.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+set -e
+
+if [ -f ./cache/oracle-12c.tar.gz ]; then
+    echo "Oracle 12c is already cached, nothing to do..."
+    exit 0
+fi
+
+cd $WORKDIR/.circleci
+
+# Download Oracle 12c Install Files
+ORACLE12c_FILE1=linuxamd64_12102_database_se2_1of2.zip
+ORACLE12c_FILE2=linuxamd64_12102_database_se2_2of2.zip
+bash download.sh -p se12c
+mv $ORACLE12c_FILE1 ./dockerfiles/12.1.0.2
+mv $ORACLE12c_FILE2 ./dockerfiles/12.1.0.2
+
+# Build and Save Docker image
+cd ./dockerfiles/12.1.0.2
+docker build -f Dockerfile.se2 --force-rm -t oracle-12c-install .
+docker run -d --privileged --name oracle-12c-install -p 1521:1521 oracle-12c-install
+docker logs -f oracle-12c-install | grep -m 1 "DATABASE IS READY TO USE!" --line-buffered
+docker exec oracle-12c-install ./setPassword.sh oracle
+docker stop oracle-12c-install
+docker commit oracle-12c-prebuilt
+docker save oracle-12c-prebuilt > ~/utplsql/.cache/oracle-12c.tar
+docker rm oracle-12c-install
+docker rmi oracle-12c-prebuilt
+docker rmi oracle-12c-install
